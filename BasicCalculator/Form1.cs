9111244939e5cdd1f589ec6ad9cbf32b68320c7c
Tinka Pirc <tinka.pirc@gmail.com>
2020-11-24 18:33:03 +0100

    User interface finished.

--- BasicCalculator/Form1.cs
+++ BasicCalculator/Form1.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/// <summary>
+/// A basic calculator
+/// </summary>
 namespace BasicCalculator
 {
     public partial class Form1 : Form
@@ -26,99 +22,326 @@ namespace BasicCalculator
 
         #region Clearing Methods
 
+        /// <summary>
+        /// Clears the user input text
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void CEButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hi");
+            // Clears the text from the user input text box
+            this.UserInputText.Text = string.Empty;
+
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Clears a character at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void DELButton_Click(object sender, EventArgs e)
         {
+            // Deletes the value after the selected position
+            DeleteTextValue();
 
+            // Focus the user input text
+            FocusInputText();
         }
 
         #endregion
 
         #region Operator functions
 
+        /// <summary>
+        /// Inserts a / at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void DivisionButton_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("/");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts a * at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void MultiplyButton_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("*");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts a - at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void MinusButton_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("-");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts a + at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void PlusButton_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("+");
 
+            // Focus the user input text
+            FocusInputText();
+        }
+
+        /// <summary>
+        /// Calculates the given equation in the user input text
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void EqualsButton_Click(object sender, EventArgs e)
+        {
+            CalculateEquation();
+
+            // Focus the user input text
+            FocusInputText();
         }
 
         #endregion
 
         #region Number methods
 
+        /// <summary>
+        /// Inserts number 7 at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void Button7_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("7");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts number 8 at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void Button8_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("8");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts number 9 at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void Button9_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("9");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts number 4 at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void Button4_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("4");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts number 5 at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void Button5_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("5");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts number 6 at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void Button6_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("6");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts number 1 at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void Button1_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("1");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts number 2 at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void Button2_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("2");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts number 3 at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void Button3_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("3");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts number 0 at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void Button0_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue("0");
 
+            // Focus the user input text
+            FocusInputText();
         }
 
+        /// <summary>
+        /// Inserts a . at certain index
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
         private void DotButton_Click(object sender, EventArgs e)
         {
+            // Inserts the value in the user input text box at the currently selected position
+            InsertTextValue(".");
 
+            // Focus the user input text
+            FocusInputText();
+        }
+
+        #endregion       
+
+        #region Private Helpers
+
+        /// <summary>
+        /// Focuses the user input text
+        /// </summary>
+        private void FocusInputText()
+        {
+            this.UserInputText.Focus();
+        }
+
+        /// <summary>
+        /// Inserts the given text into user input text box
+        /// </summary>
+        /// <param name="value"></param>
+        private void InsertTextValue(string value)
+        {
+            // Remeber selection start
+            var selectionStart = this.UserInputText.SelectionStart;
+
+            // Set new text
+            this.UserInputText.Text = this.UserInputText.Text.Insert(this.UserInputText.SelectionStart, value);
+
+            // Store new selection start
+            this.UserInputText.SelectionStart = selectionStart + value.Length;
+
+            // Set selection length to zero
+            this.UserInputText.SelectionLength = 0;
+        }
+
+        /// <summary>
+        /// Deletes the character to the right of the selection start of the user input
+        /// </summary>
+        private void DeleteTextValue()
+        {
+            // If we don't have a value to delete, return
+            if (this.UserInputText.Text.Length < this.UserInputText.SelectionStart + 1)
+                return;
+
+            // Remeber selection start
+            var selectionStart = this.UserInputText.SelectionStart;
+
+            // Delete the character to the right of the selection
+            this.UserInputText.Text = this.UserInputText.Text.Remove(this.UserInputText.SelectionStart, 1);
+
+            // Store new selection start
+            this.UserInputText.SelectionStart = selectionStart;
+
+            // Set selection length to zero
+            this.UserInputText.SelectionLength = 0;
         }
 
         #endregion
+
+        /// <summary>
+        /// Calculates the given equation and outputs the answer to the user label
+        /// </summary>
+        private void CalculateEquation()
+        {
+            // TODO: finish
+        }
     }
 }
